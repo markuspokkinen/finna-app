8c8f85e406feeb7021580b5712dfc38b5a0d4af0
Markus <markuspokkinen@gmail.com>
2019-05-07 20:28:36 +0300

    front page logic done

--- finna-app/src/App.js
+++ finna-app/src/App.js
@@ -1,27 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import FormatClass from './FormatClass';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
 
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            facets: [],
+            lang: "fi",
+            status: "non",
+            selected: 0
+        }
+    }
+
+    componentDidMount() {
+        this.getfacets();
+    }
+
+    getfacets() {
+        fetch("https://api.finna.fi/v1/search?lookfor=&facet[]=format&limit=0&lng=" + this.state.lang)
+            .then(res => res.json())
+            .then(response => {
+                this.setState({
+                    facets: response.facets.format,
+                    status: response.status
+                });
+            })
+    }
+    selectOnChange = (event) => {
+        this.setState({
+            lang: event.target.value,
+            status: "non",
+            selected :0
+        }, () => {
+            this.getfacets();
+        })
+    }
+    buttonClick = (event) => {
+        this.setState({
+            selected: parseInt(event.target.attributes.index.value)
+        })
+    }
+
+    render() {
+        let langSelect =
+            <select name="lang" onChange={this.selectOnChange}>
+                <option defaultValue>fi</option>
+                <option>en-gb</option>
+                <option>sv</option>
+            </select>;
+
+        if (this.state.status === "OK") {
+            return (
+                <div>
+                    {langSelect}
+                    <div>
+                        {this.state.facets.map((element,index) => {
+                            return <button key={element.value} index={index} onClick={this.buttonClick}>{element.translated}</button>
+                        })}
+                    </div>
+
+                    <FormatClass data={this.state.facets[this.state.selected]} />
+                </div>
+            );
+        }
+        if (this.state.status === "non") {
+            return (
+                <div>
+                    {langSelect}
+                    <div>
+                        <p>Loading...</p>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
-
-export default App;
---
+++ finna-app/src/FormatClass.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+
+export default class FormatClass extends Component {
+
+    componentDidMount() {
+        fetch("https://api.finna.fi/v1/search" + this.props.data.href)
+            .then(res => res.json())
+            .then(response => {
+                console.log(response);
+            })
+    }
+
+    render() {
+        console.log(this.props.data);
+        return (
+            <p>{this.props.data.value}</p>
+            )
+    }
+}
